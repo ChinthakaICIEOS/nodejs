84612296b4fb6678564492ca973258292f35ad99
ChinthakaICIEOS <chinthaka@icieos.com>
2025-06-27 13:35:26 +0530

    Node Lifecycle, Event Loop, Request & Response

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const http = require("http");
 
-function requestListener(request,response){
-    console.log(request);
-}
-
-const server =  http.createServer(requestListener);
+const server =  http.createServer((req,res) => {
+    console.log(req.url, req.method, req.headers);
+    // process.exit();
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head>");
+    res.write("<title>This is NodeJs Server</title>");
+    res.write("</head>");
+    res.write("<body>");
+    res.write("<h2>Hey... This is the NodeJs Server...</h2>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+});
 
 server.listen(3000);
